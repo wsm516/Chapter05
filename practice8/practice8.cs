9a7a8605d6863431e4da3c3b1bf04d445298c4ee
wsm516 <112995519+wsm516@users.noreply.github.com>
2022-12-16 21:14:14 +0900

    Update practice8.cs

--- practice8/practice8.cs
+++ practice8/practice8.cs
@@ -3,7 +3,7 @@
 abstract class Figure
 {
     public abstract void Area();      // 넓이를 구하는 메소드 
-    public abstract void Girth();     // 둘레를 구하는 메소드
+    public abstract void Girth();     // 둘레를 구하는 메소드 
     public abstract void Draw();      // 도형을 그리는 메소드 
 }
 class Squre : Figure
